e27fb088d33820723c0f8224f99c543d1e48e889
vworn <jasonkmathewpersonal@gmail.com>
2025-01-30 20:13:13 -0500

    Task 1 - Customer Discounts.

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+// Task 1: If Statements - Customer Discounts
+let purchaseAmount = 120; // Example purchase amount
+let finalAmount = purchaseAmount;
+
+if (purchaseAmount > 100) {
+    finalAmount = purchaseAmount * 0.9; // 10% discount
+}
+console.log(`Final amount after discount: $${finalAmount}`);
+
